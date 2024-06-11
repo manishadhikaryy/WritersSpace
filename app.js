303ef8ba8f484fa598ddc6a2f5343cb7540ba482
Manish Adhikari <101424844+manishadhikaryy@users.noreply.github.com>
2024-06-10 23:46:57 -0400

    Added routes and handle-bars for rendering html

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
  const express = require('express')
  const dotenv = require('dotenv')
- const connectDB = require('./config/db')
  const morgan = require('morgan')
+ const exphbs = require('express-handlebars')
+ const connectDB = require('./config/db')
+ const path = require('path')
+
+ 
+ 
 
  // Load config
  dotenv.config({ path: './config/config.env'})
@@ -14,9 +19,18 @@ if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
+// Handlebars
+app.engine('.hbs', exphbs.engine({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'));
+
+// Routes
+app.use('/', require('./routes/index'))
+
+app.use('/static', express.static(__dirname+'/static'))
+
+
 const PORT = process.env.PORT || 3000
-
-
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
